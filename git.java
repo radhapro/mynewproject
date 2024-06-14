5a084ff31918df9096c6ec2b22e6e5f879990691
radha <radha.robozz@gmail.com>
2024-06-14 14:48:15 +0530

    first commit

---
+++ git.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 10;  // First number
+        int num2 = 20;  // Second number
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of ");
+    }
+}
